aff317eaf5b99c7f1f0e92b5260d53fb3fe049d7
maqiang <maqiang@chemanman.com>
2018-11-07 14:22:20 +0800

    修正字段填充时，update失效问题

--- demo-module/src/main/java/com/github/demo/db/fill/DemoMetaObjectHandler.java
+++ demo-module/src/main/java/com/github/demo/db/fill/DemoMetaObjectHandler.java
@@ -2,10 +2,10 @@ package com.github.demo.db.fill;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.github.demo.db.fill.field.FieldFiller;
-import com.github.demo.db.fill.field.impl.UserCreateFieldFiller;
 import com.github.demo.db.fill.field.impl.GmtCreateFieldFiller;
-import com.github.demo.db.fill.field.impl.UserModifiedFieldFiller;
 import com.github.demo.db.fill.field.impl.GmtModifiedFieldFiller;
+import com.github.demo.db.fill.field.impl.UserCreateFieldFiller;
+import com.github.demo.db.fill.field.impl.UserModifiedFieldFiller;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
@@ -28,7 +28,7 @@ public class DemoMetaObjectHandler implements MetaObjectHandler {
         fillers.add(new GmtModifiedFieldFiller());
         fillers.add(new UserModifiedFieldFiller());
         for (FieldFiller filler : fillers) {
-            filler.doFiller(metaObject);
+            filler.doFiller(this, metaObject);
         }
     }
 
@@ -38,7 +38,7 @@ public class DemoMetaObjectHandler implements MetaObjectHandler {
         fillers.add(new GmtModifiedFieldFiller());
         fillers.add(new UserModifiedFieldFiller());
         for (FieldFiller filler : fillers) {
-            filler.doFiller(metaObject);
+            filler.doFiller(this, metaObject);
         }
     }
 }
